66c1d2ab593879d1566133c4d77b33ed28e987b5
adesh7061 <78601640+adesh7061@users.noreply.github.com>
2023-06-01 17:27:44 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const Post = require('./models/Post');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({credentials:true,origin:'http://localhost:3001'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
